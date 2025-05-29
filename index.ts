3c8dd1acde76d428e90086b465a35e26fdc77d96
Kimberly Gilkey <kim@gilkey.io>
2025-05-28 20:34:16 -0400

    Add tsconfig and index.ts

---
+++ index.ts
@@ -0,0 +1,7 @@
+// index.ts
+
+export type { TypeExpr } from "./expression_string";
+
+export type { Tokenize } from "./tokenize";
+export type { ToAstString } from "./parse";
+export type { Evaluate } from "./evaluate";
